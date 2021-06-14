0cf36194b2d30eb8f03dba13d9c2f781bb8c2d1d
Nick Friday <nfriday@yandex.ru>
2021-06-15 00:45:14 +0300

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+# dueVir
+
+Simple plugin that provides you due for the <YYYY-MM-DD> string.
+
+![Example](img/ex.png)
+
+## Requirements
+
+- Neovim Nightly (0.5) (may work with 0.4.4 idk test it and message me lol)
+
+## Installation
+
+I don't think any vim-ish plugin manager can install this lua shit. So it is
+[packer.nvim](https://github.com/wbthomason/packer.nvim) and similarities only...
+
+```lua
+use {
+  'NFrid/dueVir',
+  config = function()
+    require('dueVir').setup{}
+  end
+}
+```
+
+Or you may implement your own 'setup' due to mine just being couple of aucmds
+with plugin's functions (listed below).
+
+## Variables
+
+Plugin's settings are all in vim variables. There are all of them, with their
+default values, in lua, of course:
+
+```lua
+vim.g.dueVir_prescript = 'due: '      -- prescript to due data
+vim.g.dueVir_prescript_hi = 'Comment' -- highlight group of it
+vim.g.dueVir_due_hi = 'String'        -- highlight group of the data itself
+vim.g.dueVir_ft = '*.md'              -- filename to apply aucmds :)
+```
+
+## TODO
+
+idk if I ever will develop this thing more... In case of somebody needs it, here
+are my 'plans' on it:
+
+- Pattern as a setting
+- Multiple patterns
+- Settings in setup function
+- idk time?????
---
+++ lua/dueVir.lua
@@ -0,0 +1,62 @@
+local M = {}
+_VT_NS = vim.api.nvim_create_namespace("lsp_signature")
+
+local function parseDue(due)
+  local year = 31556926
+  local month = 2629743
+  local week = 604800
+  local day = 86400
+  local res = ''
+  if due >= year then
+    res = res .. math.floor(due / year) .. 'y '
+    due = due % year
+  end
+  if due >= month then
+    res = res .. math.floor(due / month) .. 'm '
+    due = due % month
+  end
+  if due >= week then
+    res = res .. math.floor(due / week) .. 'w '
+    due = due % week
+  end
+  if due >= day then res = res .. math.floor(due / day) .. 'd ' end
+  return res
+end
+
+vim.g.dueVir_prescript = 'due: '
+vim.g.dueVir_prescript_hi = 'Comment'
+vim.g.dueVir_due_hi = 'String'
+vim.g.dueVir_ft = '*.md'
+
+function M.draw(buf)
+  local now = os.time(os.date('*t'))
+  for key, value in pairs(vim.api.nvim_buf_get_lines(buf, 0, -1, {})) do
+    local date = string.match(value, '<%d%d%d%d%-%d%d%-%d%d>')
+    if date then
+      local year, month, day = date:match('<(%d%d%d%d)%-(%d%d)%-(%d%d)>')
+      local due = os.time({ year = year, month = month, day = day }) - now
+      vim.api.nvim_buf_set_virtual_text(buf, _VT_NS, key - 1, {
+        { vim.g.dueVir_prescript, vim.g.dueVir_prescript_hi },
+        { parseDue(due), vim.g.dueVir_due_hi }
+      }, {})
+    end
+  end
+end
+
+function M.clear(buf)
+  vim.api.nvim_buf_clear_namespace(buf, _VT_NS, 0, -1)
+end
+
+function M.redraw(buf)
+  M.clear(buf)
+  M.draw(buf)
+end
+
+function M.setup()
+  vim.api.nvim_command('autocmd BufEnter ' .. vim.g.dueVir_ft ..' lua require("dueVir").draw(0)')
+  vim.api.nvim_command('autocmd InsertLeave ' .. vim.g.dueVir_ft ..' lua require("dueVir").redraw(0)')
+  vim.api.nvim_command('autocmd TextChanged ' .. vim.g.dueVir_ft ..' lua require("dueVir").redraw(0)')
+  vim.api.nvim_command('autocmd TextChangedI ' .. vim.g.dueVir_ft ..' lua require("dueVir").redraw(0)')
+end
+
+return M
