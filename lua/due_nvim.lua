9d796518b6f36ff357c5d7114f05e170e52fe5f5
Nick Friday <nfriday@yandex.ru>
2021-07-04 12:01:52 +0300

    fix: clock time default to false and no global var

--- lua/due_nvim.lua
+++ lua/due_nvim.lua
@@ -1,6 +1,25 @@
 local M = {}
 _VT_NS = vim.api.nvim_create_namespace("lsp_signature")
 
+local prescript
+local prescript_hi
+local due_hi
+local ft
+local today
+local today_hi
+local overdue
+local overdue_hi
+local date_hi
+local pattern_start
+local pattern_end
+
+local use_clock_time
+local default_due_time
+
+local date_pattern
+local fulldate_pattern
+local date_pattern_match
+local fulldate_pattern_match
 
 local function parseDue(due)
   local year = 31556926
@@ -30,17 +49,17 @@ local function parseDue(due)
     if due >= day then
       res = res .. math.floor(due / day) .. 'd '
       due = due % day
-    end  
+    end
 
     if due >= hour then
       res = res .. math.floor(due / hour) .. 'h '
       due = due % hour
-    end 
+    end
 
     if due >= minute then
       res = res .. math.floor(due / minute) .. 'min '
       due = due % minute
-    end  
+    end
 
     res = res .. math.floor(due / 1) + 1 .. 's '
 
@@ -51,27 +70,8 @@ local function parseDue(due)
   return res
 end
 
-
-local prescript
-local prescript_hi
-local due_hi
-local ft
-local today
-local today_hi
-local overdue
-local overdue_hi
-local date_hi
-local pattern_start
-local pattern_end
-
-local date_pattern
-local fulldate_pattern
-local date_pattern_match
-local fulldate_pattern_match
-
-
 function M.setup(c)
-  use_clock_time = c.use_clock_time or true
+  use_clock_time = c.use_clock_time or false
   default_due_time = c.default_due_time or 'midnight'
   prescript = c.prescript or 'due: '
   prescript_hi = c.prescript_hi or 'Comment'
@@ -97,17 +97,24 @@ function M.setup(c)
   date_pattern_match = lua_start .. '(%d%d)%-(%d%d)' .. lua_end
   fulldate_pattern_match = lua_start .. '(%d%d%d%d)%-(%d%d)%-(%d%d)' .. lua_end
 
-  local regex_hi = '/' .. regex_start .. '\\d*-*\\d\\+-\\d\\+' .. regex_end .. '/'
+  local regex_hi = '/' .. regex_start .. '\\d*-*\\d\\+-\\d\\+' .. regex_end ..
+                       '/'
 
-  vim.api.nvim_command('autocmd BufEnter ' .. ft ..' lua require("due_nvim").draw(0)')
-  vim.api.nvim_command('autocmd InsertLeave ' .. ft ..' lua require("due_nvim").redraw(0)')
-  vim.api.nvim_command('autocmd TextChanged ' .. ft ..' lua require("due_nvim").redraw(0)')
-  vim.api.nvim_command('autocmd TextChangedI ' .. ft ..' lua require("due_nvim").redraw(0)')
+  vim.api.nvim_command('autocmd BufEnter ' .. ft ..
+                           ' lua require("due_nvim").draw(0)')
+  vim.api.nvim_command('autocmd InsertLeave ' .. ft ..
+                           ' lua require("due_nvim").redraw(0)')
+  vim.api.nvim_command('autocmd TextChanged ' .. ft ..
+                           ' lua require("due_nvim").redraw(0)')
+  vim.api.nvim_command('autocmd TextChangedI ' .. ft ..
+                           ' lua require("due_nvim").redraw(0)')
 
-  vim.api.nvim_command('autocmd BufEnter ' .. ft .. ' syn match DueDate ' .. regex_hi .. ' display containedin=mkdNonListItemBlock,mkdListItemLine,mkdBlockquote contained')
-  vim.api.nvim_command('autocmd BufEnter ' .. ft .. ' hi def link DueDate ' .. date_hi)
+  vim.api.nvim_command('autocmd BufEnter ' .. ft .. ' syn match DueDate ' ..
+                           regex_hi ..
+                           ' display containedin=mkdNonListItemBlock,mkdListItemLine,mkdBlockquote contained')
+  vim.api.nvim_command('autocmd BufEnter ' .. ft .. ' hi def link DueDate ' ..
+                           date_hi)
 end
-
 
 function M.draw(buf)
   local user_time
@@ -117,15 +124,15 @@ function M.draw(buf)
 
   -- get user default time option
   if default_due_time == "midnight" then
-    user_hour = 23 
-    user_min = 59 
-    user_sec = 59  
+    user_hour = 23
+    user_min = 59
+    user_sec = 59
   elseif default_due_time == "noon" then
-    user_hour = 12 
-    user_min = 00 
-    user_sec = 00   
+    user_hour = 12
+    user_min = 00
+    user_sec = 00
   end
-  
+
   -- get current time
   local now = os.time(os.date('*t'))
 
@@ -139,7 +146,14 @@ function M.draw(buf)
       local month, day = date:match(date_pattern_match)
       local cur_year = os.date("%Y")
 
-      user_time = os.time({ year = cur_year, month = month, day = day, hour = user_hour, min = user_min, sec = user_sec }) 
+      user_time = os.time({
+        year = cur_year,
+        month = month,
+        day = day,
+        hour = user_hour,
+        min = user_min,
+        sec = user_sec
+      })
 
       due = user_time - now
     end
@@ -147,7 +161,14 @@ function M.draw(buf)
     if fullDate then
       local cur_year, month, day = fullDate:match(fulldate_pattern_match)
 
-      user_time = os.time({ year = cur_year, month = month, day = day, hour = user_hour, min = user_min, sec = user_sec }) 
+      user_time = os.time({
+        year = cur_year,
+        month = month,
+        day = day,
+        hour = user_hour,
+        min = user_min,
+        sec = user_sec
+      })
 
       due = user_time - now
     end
@@ -164,16 +185,13 @@ function M.draw(buf)
       end
 
       vim.api.nvim_buf_set_virtual_text(buf, _VT_NS, key - 1, {
-        { prescript, prescript_hi },
-        parsed
-    }, {})
+        { prescript, prescript_hi }, parsed
+      }, {})
     end
   end
 end
 
-function M.clear(buf)
-  vim.api.nvim_buf_clear_namespace(buf, _VT_NS, 0, -1)
-end
+function M.clear(buf) vim.api.nvim_buf_clear_namespace(buf, _VT_NS, 0, -1) end
 
 function M.redraw(buf)
   M.clear(buf)
